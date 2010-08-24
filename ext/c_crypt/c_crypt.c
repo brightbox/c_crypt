81ed6c6fe9ca35a8c50cb09b2ee95fb7617a4ffb
Caius Durling <caius@brightbox.co.uk>
2010-08-24 21:20:55 +0000

    Adding the C code

---
+++ ext/c_crypt/c_crypt.c
@@ -0,0 +1,34 @@
+#include <ruby.h>
+#include <crypt.h>
+
+// Define our module constant
+VALUE CCrypt = Qnil;
+
+// Prototype this
+void Init_c_crypt();
+
+// Prototype CCrypt.crypt3
+VALUE method_crypt3();
+
+// Define our module & method
+void Init_c_crypt() {
+  CCrypt = rb_define_module("CCrypt");
+  int arg_count = 2;
+  rb_define_module_function(CCrypt, "crypt3", method_crypt3, arg_count);
+}
+
+// Implement CCrypt.crypt3
+VALUE method_crypt3(VALUE self, VALUE str_arg, VALUE salt_arg) {
+  char* str = RSTRING(str_arg)->ptr;
+  char* salt = RSTRING(salt_arg)->ptr;
+
+//  printf("str: %s\n", str);
+//  printf("salt: %s\n", salt);
+
+  char *hashed = crypt(str, salt);
+
+//  printf("hashed: %s\n", hashed);
+  
+  return rb_str_new2(hashed);
+}
+
---
+++ ext/c_crypt/extconf.rb
@@ -0,0 +1,6 @@
+require 'mkmf'
+
+dir_config("c_crypt")
+
+create_makefile("c_crypt")
+
